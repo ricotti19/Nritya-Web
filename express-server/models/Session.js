1d82dae252c1697606dc392689ea8f08d11d6026
Shriya Shenoj <SHENOJSHRIYA19@GMAIL.COM>
2026-05-12 11:00:43 -0500

    Fix comment formatting in Session.js

--- express-server/models/Session.js
+++ express-server/models/Session.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const SessionSchema = new mongoose.Schema({
   danceStep: String,     // e.g., "Aramandi Squat"
   difficulty: Number,    // 1-10
-  accuracyScore: Number, // From your MediaPipe logic
+  accuracyScore: Number, // From MediaPipe logic
   date: { type: Date, default: Date.now }
 });
 
